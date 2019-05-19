35a2f6a2294cbe853ccdf98ab43c3da5d692d746
maksimp04 <maksimp04@mail.ru>
2019-05-19 11:59:51 +0600

    Add files via upload

--- timerscript.js
+++ timerscript.js
@@ -1,4 +1,5 @@
-﻿var DayX = new Date(2019,7,15,15,0,0,0);
+﻿var DayX = new Date(2022,10,10,0,40,0,0);
+var Years = DayX.getFullYear() - new Date().getFullYear()
 var Days = DayX.getDay() + (DayX.getMonth() - new Date().getMonth())*28;
 var Hours = DayX.getHours() - new Date().getHours();
 var Minutes = DayX.getMinutes() - new Date().getMinutes();
@@ -8,7 +9,7 @@ if (Minutes < 0) {
 }
 if (Minutes > 60) {
 	Minutes = Minutes - 60;
-	Hours = Hours + 1
+	Hours = Hours + 1;
 }
 if (Hours < 0) {
 	Hours = Hours + 24;
@@ -18,4 +19,4 @@ if (Hours > 24) {
 	Hours = Hours - 24
 	Days = Days + 1
 }
-document.getElementById("timer").innerHTML=Days + " дней, " + Hours + " часов, " + Minutes + " минут.";
+document.getElementById("timer").innerHTML=Years + " лет " + Days + " дней, " + Hours + " часов, " + Minutes + " минут.";
