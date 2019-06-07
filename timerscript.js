6d0907353f47518099619c7261afbc63d7af7cea
maksimp04 <maksimp04@mail.ru>
2019-06-07 14:06:23 +0600

    Add files via upload

--- timerscript.js
+++ timerscript.js
@@ -1,9 +1,19 @@
 ﻿var DayX = new Date(2022,10,10,0,40,0,0);
+
 var Years = DayX.getFullYear() - new Date().getFullYear()
-var Days = DayX.getDay() + (DayX.getMonth() - new Date().getMonth())*28;
+var Days = DayX.getMonth() * 29 + DayX.getDate() - new Date().getMonth() * 29 + new Date().getDate();
 var Hours = DayX.getHours() - new Date().getHours();
 var Minutes = DayX.getMinutes() - new Date().getMinutes();
 var Seconds = DayX.getSeconds() - new Date().getSeconds();
+
+var DayZ = new Date(2018,8,5,12,39,45,0);
+var ZYears = new Date().getFullYear() - DayZ.getFullYear();
+var ZDays = new Date().getMonth() * 29 + new Date().getDate() - DayZ.getMonth() * 29 + DayZ.getDate();
+var ZHours = new Date().getHours() - DayZ.getHours();
+var ZMinutes = new Date().getMinutes() - DayZ.getMinutes();
+var ZSeconds = new Date().getSeconds() - DayZ.getSeconds();
+
+
 if (Seconds < 0) {
 	Minutes = Minutes - 1
 	Seconds = Seconds + 60
@@ -24,10 +34,38 @@ if (Hours > 24) {
 	Hours = Hours - 24
 	Days = Days + 1
 }
+if (ZSeconds < 0) {
+	ZMinutes = ZMinutes - 1
+	ZSeconds = ZSeconds + 60
+}
+if (ZMinutes < 0) {
+	ZMinutes = ZMinutes + 60;
+	ZHours = ZHours - 1;
+}
+if (ZMinutes > 60) {
+	ZMinutes = ZMinutes - 60;
+	ZHours = ZHours + 1;
+}
+if (ZHours < 0) {
+	ZHours = ZHours + 24;
+	ZDays = ZDays - 1;
+}
+if (ZHours > 24) {
+	ZHours = ZHours - 24
+	ZDays = ZDays + 1
+}
+if (ZDays <0) {
+	ZDays = ZDays + 365
+	ZYears = ZYears - 1
+}
+
 document.getElementById("timer").innerHTML=Years + " лет " + Days + " дней, " + Hours + " часов, " + Minutes + " минут, " + Seconds + " секунд.";
 setInterval(secondss,1000);
+setInterval(moveImage,600);
+setInterval(changeImage,3000);
 function secondss() {
 	Seconds = Seconds - 1;
+	ZSeconds = ZSeconds + 1;
 	if (Seconds < 0) {
 		Minutes = Minutes - 1
 		Seconds = Seconds + 60
@@ -51,5 +89,34 @@ function secondss() {
 		Hours = Hours - 24
 		Days = Days + 1
 	}
+	if (ZSeconds > 59) {
+		ZMinutes = ZMinutes + 1
+		ZSeconds = ZSeconds - 60
+	}
+	if (ZMinutes < 0) {
+		ZMinutes = ZMinutes + 60;
+		ZHours = ZHours - 1;
+	}
+	if (ZMinutes > 60) {
+		ZMinutes = ZMinutes - 60;
+		ZHours = ZHours + 1;
+	}
+	if (ZHours < 0) {
+		ZHours = ZHours + 24;
+		ZDays = ZDays - 1;
+	}
+	if (ZHours > 24) {
+		ZHours = ZHours - 24
+		ZDays = ZDays + 1
+	}
 	document.getElementById("timer").innerHTML=Years + " лет " + Days + " дней, " + Hours + " часов, " + Minutes + " минут, " + Seconds + " секунд.";
+	if (ZYears > 0) {
+		document.getElementById("timerclose").innerHTML=ZYears + " год " + ZDays + " дней, " + ZHours + " часов, " + ZMinutes + " минут, " + ZSeconds + " секунд.";
+	}
+	else {
+		document.getElementById("timerclose").innerHTML=ZDays + " дней, " + ZHours + " часов, " + ZMinutes + " минут, " + ZSeconds + " секунд.";
+	}
+}
+function changeImage() {
+	document.getElementById("image")
 }
