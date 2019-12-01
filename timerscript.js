34b603f697a41859216304935331ef3c1c6fbe5a
maksimp04 <maksimp04@mail.ru>
2019-12-01 22:39:01 +0600

    Update timerscript.js

--- timerscript.js
+++ timerscript.js
@@ -70,10 +70,10 @@ else {
 	document.getElementById("timer").innerHTML=Days + " дней, " + Hours + " часов, " + Minutes + " минут, " + Seconds + " секунд.";
 }
 if (ZYears > 0) {
-	document.getElementById("timerclose").innerHTML=ZYears + " год " + ZDays + " дней, " + ZHours + " часов, " + ZMinutes + " минут, " + ZSeconds + " секунд.";
+	document.getElementById("timerclose").innerHTML=ZYears + "г " + ZDays + "д " + ZHours + "ч " + ZMinutes + "м " + ZSeconds + "с";
 }
 else {
-	document.getElementById("timerclose").innerHTML=ZDays + " дней, " + ZHours + " часов, " + ZMinutes + " минут, " + ZSeconds + " секунд.";
+	document.getElementById("timerclose").innerHTML=ZDays + "д " + ZHours + "ч " + ZMinutes + "м " + ZSeconds + "с";
 }
 setInterval(secondss,1000);
 setInterval(moveImage,600);
